161f9b8f1bc3cb3b6918f03206c5c233c0ad8d70
enrique garcia <enriquegarcia259@yahoo.es>
2019-12-05 03:08:45 -0600

    Iniciando login

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,17 @@
 package sample;
 
+import javafx.scene.image.Image;
+
+import javax.swing.text.html.ImageView;
+import java.awt.*;
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class Controller {
+
+
+
+
+
 }
