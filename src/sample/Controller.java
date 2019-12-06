62a6b4c7882bd44a55702901e6c8eae8411687c3
Aruka <jamesmeza667@gmail.com>
2019-12-05 19:01:30 -0600

    Conexion entre scenes (Formularios)

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,31 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class Controller {
+    @FXML
+    private Button BtnLogin;
+
+
+    public void onPasarVentana(MouseEvent mouseEvent) throws IOException {
+        Stage stage = null;
+        Parent rootPrincipal = null;
+
+        if(mouseEvent.getSource() == BtnLogin){
+            stage = (Stage) BtnLogin.getScene().getWindow();
+
+            rootPrincipal = FXMLLoader.load(getClass().getResource("../Interfaz_Menu/Controlador_Grafico.fxml"));
+            Scene ScenePrincipal = new Scene(rootPrincipal);
+            stage.setScene(ScenePrincipal);
+            stage.show();
+        }
+    }
 }
