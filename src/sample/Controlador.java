601fec156d3b4393981d1f22539ee673b54dcaea
adan <adanalfredogaleascarcamo@gmail.com>
2019-12-06 22:40:21 -0600

    Iniciando diseños de registro cliente y abogado

--- src/sample/Controlador.java
+++ src/sample/Controlador.java
@@ -1,18 +1,21 @@
-package Interfaz_Menu;
+package sample;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.awt.event.ActionEvent;
+
 public class Controlador {
+
     public void IrFormulario () {
         try {
 
 
 //Te lo comento para que  lo comprendamos tú y yo
 //Léeme el source del archivo que te digo fxml y te pongo el path
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("path_to.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Controlador_Grafico.fxml"));
             Parent root1 = (Parent) fxmlLoader.load();
 
 //Creame un nuevo Stage o para que nos entendamos una nueva ventana windows vacía
