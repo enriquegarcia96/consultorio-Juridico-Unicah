65f1df2b086aad63dc6f2412edf8ea9ac3d5a645
adan <adanalfredogaleascarcamo@gmail.com>
2019-12-06 19:50:28 -0600

    Conexion

--- src/sample/Conexion.java
+++ src/sample/Conexion.java
@@ -1,6 +1,5 @@
 package sample;
 import java.sql.*;
-<<<<<<< HEAD
 
 public class Conexion {
 
@@ -11,8 +10,8 @@ public class Conexion {
     public static Connection abrirConexion() {
         if (con == null) {
             try {
-                con = DriverManager.getConnection("jdbc:mysql://localhost/consultorio_juridico_unicah?useTimezone=true&serverTimezone=UTC",
-                        "root", "Dragon97");
+                con = DriverManager.getConnection("jdbc:mysql://localhost/consultorio?useTimezone=true&serverTimezone=UTC",
+                        "root", "dinorey64");
             } catch (SQLException e) {
                 System.err.println(e.getMessage());
             }
@@ -22,24 +21,3 @@ public class Conexion {
 
 
 }
-=======
-import javax.swing.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-public class Conexion {
-    private static Connection Connect;
-    public void MySQLConnection(String user, String pass, String db_name) throws Exception {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connect = DriverManager.getConnection("jdbc:mysql://localhost/consultorio_juridico_unicah?useTimezone=true&serverTimezone=UTC", "root", "Dragon97");
-            JOptionPane.showMessageDialog(null, "Se ha iniciado la conexión con el servidor de forma exitosa");
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (SQLException ex) {
-            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-}
-
->>>>>>> master
