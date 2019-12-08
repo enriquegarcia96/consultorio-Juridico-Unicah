b7e3f18ef357a22f95e7d26c0e78bc8de06127f8
enrique garcia <enriquegarcia259@yahoo.es>
2019-12-07 21:25:44 -0600

    controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,19 +1,13 @@
 package sample;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-
 import javafx.stage.Stage;
 
 import java.sql.PreparedStatement;
